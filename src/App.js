847827f3b6f654f7ad7bbd6aa7501b19c95b6729
Krishna Khairnar <krishnakhairnar7@gmail.com>
2024-04-26 23:15:52 +0530

    Adds routers for TextUtils application.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes, 
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
@@ -36,13 +42,21 @@ function App() {
 
   return (
     <>
+    <Router>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />
-        {/* <About /> */}
-      </div>
       
+      <div className="container my-3">
+
+        <Routes>
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />}>
+            </Route>
+            <Route exact path="/about" element={<About mode={mode} />}>
+            </Route>
+        </Routes>
+        
+      </div>
+    </Router>
     </>
   );
 }
